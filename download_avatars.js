f1bf83bdb4cd689792f0fec6194a3293a0d5ae8e
SylvainJunca <sylvain.junca@gmail.com>
2018-10-29 19:32:34 -0400

    Connects my main function to the one that downloads the avatars

--- download_avatars.js
+++ download_avatars.js
@@ -7,8 +7,8 @@ const key = require('./secret');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)               // Note 1
-  .on('error', function (err) {                                   // Note 2
+  request.get(url)              
+  .on('error', function (err) {                                  
   throw err;
   })
   .pipe(fs.createWriteStream(filePath)); 
@@ -29,12 +29,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
           console.error(err);
       };
     const content = JSON.parse(body);
-    for(var each of content) {
-    console.log(each['avatar_url']);
-    }
+    cb(content);
+    
   });
 }
-getRepoContributors('jquery', 'jquery');
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
-  
-  
+getRepoContributors('jquery', 'jquery', function(content) {
+    for(var each of content) {
+        downloadImageByURL(each['avatar_url'], `./avatars/${each.login}.jpg`);
+        }
+});
