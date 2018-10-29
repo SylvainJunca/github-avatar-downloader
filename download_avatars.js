ca0e786db96d00cc9b645e7595cbc51856adb258
SylvainJunca <sylvain.junca@gmail.com>
2018-10-29 18:46:31 -0400

    Integrates a working solution for the downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 var request = require('request');
+var fs = require("fs");
 
 const key = require('./secret');
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)               // Note 1
+  .on('error', function (err) {                                   // Note 2
+  throw err;
+  })
+  .pipe(fs.createWriteStream(filePath)); 
+
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = { 
@@ -25,5 +35,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 getRepoContributors('jquery', 'jquery');
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
   
   
