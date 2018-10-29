2d1b4b40e6ece01eea3faceecc4bc58a68313695
SylvainJunca <sylvain.junca@gmail.com>
2018-10-29 18:34:44 -0400

    Integrates the first interaction with the JSON. Gets the url of each avatar and prints it

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,29 @@
 var request = require('request');
 
+const key = require('./secret');
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
-    
-  }
+  var options = { 
+    Authorization: "token " + key,
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+    'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+      if (err) {
+          console.error(err);
+      };
+    const content = JSON.parse(body);
+    for(var each of content) {
+    console.log(each['avatar_url']);
+    }
+  });
+}
+getRepoContributors('jquery', 'jquery');
   
   
